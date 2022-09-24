a990684ca5a69210aed69d0856bda4402dd34035
t3bol90 <43676770+t3bol90@users.noreply.github.com>
2022-09-24 09:09:01 +0700

    Calibration

--- build-protoc.sh
+++ build-protoc.sh
@@ -1,1 +1,2 @@
+
 python -m grpc_tools.protoc -I./proto --python_out=./proto/ --grpc_python_out=./proto/ ./proto/sample.proto
---
+++ proto/build-protoc.sh
@@ -0,0 +1,2 @@
+python -m grpc_tools.protoc -I./ --python_out=./ --grpc_python_out=./ ./sample.proto
+
